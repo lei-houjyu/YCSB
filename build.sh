55b02df7a726352716998f9c8370b6cfe202d31e
Haoyu Li <haoyu.li@columbia.edu>
2021-06-03 09:20:45 -0600

    Fix a bug in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 mvn -pl rocksdb -am clean package  -Dmaven.test.skip=true
 rm rocksdb/target/dependency/rocksdbjni-6.2.2.jar
-cp with-compaction.jar rocksdb/target/dependency/
+cp with-compaction.jar rocksdb/target/dependency/rocksdbjni-6.14.0.jar
